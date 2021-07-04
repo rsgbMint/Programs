4d53a7f3dd32ce88b7f98f6ddff72044f04c030b
Roly24 <thanhecan@gmail.com>
2021-07-04 13:02:12 -0500

    Uploading List_Ccmprehensions.py file

--- Python/Practical_Introduction_to_Python/List_Comprehensions.py
+++ Python/Practical_Introduction_to_Python/List_Comprehensions.py
@@ -3,3 +3,21 @@
 # Here is a simple example
 L = [i*i for i in range(5)]
 print(L)
+
+#############################################################
+# Example 1
+string = 'Hello'
+output = [c*2 for c in string]
+print(output)
+
+# Example 1 with the long way
+L = []
+for c in string:
+    L.append(c*2)
+print(L)
+
+#############################################################
+# Example 3
+M = ['one', 'two', 'three', 'four', 'five', 'six']
+output = [m[-1] for m in M]
+print(output)
