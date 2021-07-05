4bb56c6bd50f3d91ee63b151c7e4f8f1b890ecb8
Roly24 <thanhecan@gmail.com>
2021-07-04 22:43:38 -0500

    Uploadind Default.py file

---
+++ Python/Practical_Introduction_to_Python/Default.py
@@ -0,0 +1,12 @@
+# Default arguments neet to come at the end of the
+# function definition, after all the non-default
+# arguments.
+def multiple_print(string, n=1):
+    print(string * n)
+    print()
+
+multiple_print('Hello', 5)
+multiple_print('Hello')
+
+
+
