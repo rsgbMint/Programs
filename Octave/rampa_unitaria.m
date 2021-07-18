ef015b4c9f3d07656813a4f188c55190c35d3870
Roly24 <thanhecan@gmail.com>
2021-07-17 23:54:10 -0500

    Uploading rampa_unitaria.m file

--- Octave/rampa_unitaria.m
+++ Octave/rampa_unitaria.m
@@ -10,14 +10,23 @@ m = 1;
 r1 = (t>=0).*t*m;
 
 % Second part
-r2 = (t>=1).*(t-1)*-m;
+r2 = (t>=0).*(t-1)*-m;
 
 % The final function
 rt = r1 + r2;
 
 % For plotting the function
 figure 1
-plot(t,rt,'linewidth',2)
+subplot(1,2,1)
+plot(t,r1,'linewidth',2)
 grid on
+subplot(1,2,2)
+plot(t,r2,'linewidth',2)
+grid on
+
+figure 2
+plot(t,r1,'linewidth',2)
+hold on
+plot(t,r2,'linewidth',2)
 
 pause
