e87c1593225b903a59636a1f8803dcfc934e35fd
Roly24 <thanhecan@gmail.com>
2021-07-22 23:44:09 -0500

    Updating area_under_a_curve.py file

--- Python/Scipy/Numerical_integration.py
+++ Python/Scipy/Numerical_integration.py
@@ -1,13 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+# a and b are the limits
 a = 0; b =1
+
+# Number of rectangles 
 N =10
 
+# Are the x-axis values
 x = np.linspace(a,b, N +1)
 
+# The function
 y = x**2;
 
+# Are the y-axis values
 y_right = y[1:]
 y_left = y[:-1]
 
@@ -15,4 +20,5 @@ y_left = y[:-1]
 dx = (b - a)/N
 A = (dx/2)*np.sum(y_right + y_left)
 
+# To show the area
 print('A =',A)
