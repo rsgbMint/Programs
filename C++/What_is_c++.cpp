da288d870c977930ddbcce0f551e5b4247e15956
Roly24 <thanhecan@gmail.com>
2021-07-25 23:22:43 -0500

    Uploading What_is_c++.cpp file

---
+++ C++/What_is_c++.cpp
@@ -0,0 +1,21 @@
+/* 
+What is C++?
+The predecessor of C++ is C, which was designed by Dennis Richie al Bell Labs and fisrt
+released in 1973
+
+In  1983, Bjarne Stroustrup released C++. The ++ operator comes from the C increment operator ++.
+
+Compilers
+C++ is a high-level language, designed to give you a wealth of language facilities and to 
+be readable for you and other developers. C++ allows you to split your code into functions,
+which takes parameters and return value, so the memory used to pass this data
+
+Linking the code
+The compiler will produce an output. For C++ code, this will be object code, but you may
+have other compiler outputs, such as compiled resource files.
+
+Source files
+At the very basic level, a C++ project will contain just one file: the C++ source file, typically
+with the extension cpp or cxx.
+
+*/
