2a4e0888c7f946dc8e89cea273e7474873c5602a
Roly24 <thanhecan@gmail.com>
2021-07-27 00:22:01 -0500

    Updating A_more_practical_example.py file

--- Python/Practical_Introduction_to_Python/Creating_my_own_classes/A_more_practical_example.py
+++ Python/Practical_Introduction_to_Python/Creating_my_own_classes/A_more_practical_example.py
@@ -12,7 +12,6 @@ class Analyzer:
     
     def starts_with(self, s):
         return len([w for w in self.words if w[:len(s)]==s])
-    
     def number_with_lenght(self, n):
         return len([w for w in self.words if len(w)==n])
 
