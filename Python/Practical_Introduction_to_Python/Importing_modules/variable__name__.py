3c2d7e0aabb3b78fabddb82671d18889ed0c486c
rsgbMint <thanhecan@gmail.com>
2021-09-30 10:13:34 -0500

    Uploading variable__name__.py file

---
+++ Python/Practical_Introduction_to_Python/Importing_modules/variable__name__.py
@@ -0,0 +1,9 @@
+# Import test_to_import file
+import test_to_import
+
+# When the interpreter runs a module, the
+# __name__ variable will be set as __main__
+# if the module that is being run is the main program
+
+# Python file one module
+print("variable name file __name__ is set to: {}".format(__name__))
