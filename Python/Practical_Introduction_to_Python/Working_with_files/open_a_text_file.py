49d463751696a1d95202963114aa8b90a91abd30
rsgbMint <thanhecan@gmail.com>
2021-10-06 01:16:43 -0500

    writting a file

--- Python/Practical_Introduction_to_Python/Working_with_files/open_a_text_file.py
+++ Python/Practical_Introduction_to_Python/Working_with_files/open_a_text_file.py
@@ -2,9 +2,17 @@ import os
 
 # To read a file named data.txt. We use the try and except block
 # in order to handle the FilenotFoundError exception 
+
 try:
+    # open() takes a filename and a mode as its arguments.
+    # Opens the file in read only mode.
     with open('data.txt', 'r') as f:
         data = f.read()
+        print(data)
 except FileNotFoundError as error:
     print(error)
     print('There is no file named data.txt')
+
+with open('data.txt', 'w') as f:
+    data = 'Some data to be written to the file'
+    f.write(data)
