99b67adafcdbe28c0d8ee13a6e7542ac938f4e98
rsgbMint <thanhecan@gmail.com>
2021-10-09 22:56:26 -0500

    Reading CSV Files into Dictionary with csv

--- Python/Practical_Introduction_to_Python/Working_with_files/reading_csv_files.py
+++ Python/Practical_Introduction_to_Python/Working_with_files/reading_csv_files.py
@@ -3,6 +3,7 @@ import csv
 # Reading CSV Files with csv
 # Opens a csv file
 with open('csv_file.csv') as f:
+
     # Reads a csv file with the Python's built-in reader() function
     # row by row.
     csv_reader = csv.reader(f, delimiter=',')
@@ -11,4 +12,14 @@ with open('csv_file.csv') as f:
     # data found by removing the delimeters.
     for row in csv_reader:
         print(row)
-        
+
+# Reading CSV Files into Dictionary with csv
+with open('csv_file.csv') as f:
+
+    # Reads a csv file with as a dictionary where the keys are the
+    # first line of the CSV file.
+    csv_reader = csv.DictReader(f)
+
+    # row variable returns a dict wher the keys are the first line or row
+    for row in csv_reader:
+        print(type(row))
