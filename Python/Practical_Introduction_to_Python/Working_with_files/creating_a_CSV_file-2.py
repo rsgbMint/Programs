1165fab70b64eb5b97bf85b0704efadf3b96d593
rsgbMint <thanhecan@gmail.com>
2021-10-10 02:36:36 -0500

    CSV file example 2 finished

---
+++ Python/Practical_Introduction_to_Python/Working_with_files/creating_a_CSV_file-2.py
@@ -0,0 +1,15 @@
+import csv
+
+import csv
+
+with open('names.csv', 'w', newline='') as csvfile:
+    fieldnames = ['first_name', 'last_name']
+    writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+    # Writes the dictionary's keys as a row.
+    writer.writeheader()
+
+    # Adds the dictionary's values to their respective keys.
+    writer.writerow({'first_name': 'Baked', 'last_name': 'Beans'})
+    writer.writerow({'first_name': 'Lovely', 'last_name': 'Spam'})
+    writer.writerow({'first_name': 'Wonderful', 'last_name': 'Spam'})
