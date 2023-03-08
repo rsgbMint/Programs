99e1d099595b38f6a05bfc16d68f9ecc1c725c58
rsgbMint <thanhecan@gmail.com>
2023-03-07 21:44:28 -0500

    Uploading new directories

---
+++ Java/sumOfTwoNumbers/Sum.java
@@ -0,0 +1,5 @@
+class Sum{
+	public static void main(String[] args){
+		num1 = 
+	}
+}
---
+++ Java/variables/Variable.java
@@ -0,0 +1,23 @@
+import javax.swing.plaf.synth.SynthTextAreaUI;
+
+class Variable {
+	public static void main(String[] args) {
+
+		// Define variables
+		int num = 10;
+		String name = "Roly";
+		char letter = 'a';
+		double temperature = 35.5;
+		boolean flag = false;
+		long large = 123456789;
+
+		// Print out the variables
+		System.out.println(num);
+		System.out.println(name);
+		System.out.println(letter);
+		System.out.println(temperature);
+		System.out.println(flag);
+		System.out.println(large);
+	}
+
+}
